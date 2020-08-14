266acf9b74d4f1d4b18febb177530dcf3e35df2a
elle-mybizniche <elle@mybizniche.com>
2020-08-15 06:11:15 +0800

    made some changes
    contact us/
    template plain blocks
    inner css

--- templates/plain-block.php
+++ templates/plain-block.php
@@ -1,19 +1,33 @@
 <?php
 /**
-	Template Name: Block - No Subs Template
+	Template Name: Centered Content Template
  */
 
 get_header();
 ?>
 
 <main id="content">
-	<?php
-	while ( have_posts() ) : the_post();
-		
-		the_content();
 
-	endwhile; // End of the loop.
-	?>
+	<div class="wp-block-group sec-banner">
+        <div class="wp-block-group__inner-container">
+            <h1 class="hbg"><?php the_title(); ?></h1>
+        </div>
+    </div>
+
+    <div class="grid-container">
+ 		<div class="grid-x grid-margin-x align-center">
+    		<div class="cell small-10 padding-vertical-3">
+				<?php
+					while ( have_posts() ) : the_post();
+						
+						the_content();
+
+					endwhile; // End of the loop.
+				?>
+    		</div>
+    	</div>
+    </div>
+	
 </main>
 
 <?php

7ff44fb0553ccf7556ac2aebd282cf9370a94872
elle-mybizniche <elle@mybizniche.com>
2020-09-02 14:56:22 +0800

    Update rep-case.php

--- templates/rep-case.php
+++ templates/rep-case.php
@@ -19,83 +19,133 @@ get_header();
     <div class="grid-container margin-vertical-3">
         <div class="grid-x grid-margin-x rep-case-lists">
 
-            <?php 
-                $attorney =  array(
-                    'post_type' => 'attorney',
-                    'post_status' => 'publish',
-                    'posts_per_page' => -1,
-                );
+            <?php if (isset($_GET['case-attorney'])): ?>
 
-                if (isset($_GET['case-attorney'])) {
-                    $attorney = array(
+                <?php 
+                     $attorney = array(
                         'post_type' => 'attorney',
                         'post_status' => 'publish',
                         'p' => $_GET['case-attorney'],
                         'posts_per_page' => -1,
                     );
-                }
 
-                $uniqueCase = [];
+                    $uniqueCase = [];
 
-                $query = new WP_Query( $attorney );
-                while ($query->have_posts()) : $query->the_post();
-            ?>
-                <?php 
-                     $repCase = get_field('fa_representative_cases_items');
+                    $query = new WP_Query( $attorney );
+                    while ($query->have_posts()) : $query->the_post();
+                        $repCase = get_field('fa_representative_cases_items');
 
-                     foreach( $repCase as $post ):  setup_postdata($post);
+                        foreach( $repCase as $post ):  setup_postdata($post);
 
-                        
                  ?>
-                    <?php if (!in_array(get_the_ID(), $uniqueCase)): ?>
-                        <?php $uniqueCase[] = get_the_ID(); ?>
 
-                        <div class="medium-6 large-6 cell grid-item">
-                            <div class="box grid-x grid-padding-x margin-bottom-2">
-                                <div class="cell small-6">
-                                    <div class="thumb">
-                                        <?php $controlImageBefore = get_field('crf_before_image'); ?>
-                                        <?php if ($controlImageBefore): ?>
-                                            <img src="<?= esc_url( $controlImageBefore['url'] ); ?>" alt="<?= esc_attr( $controlImageBefore['alt'] ); ?>"  title="<?= esc_attr( $controlImageBefore['alt'] ); ?>">
-                                        <?php else: ?>
-                                            <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
-                                        <?php endif ?>
-                                        
-                                        <span>Before</span>
+                        <?php if (!in_array(get_the_ID(), $uniqueCase)): ?>
+                            <?php $uniqueCase[] = get_the_ID(); ?>
+
+                            <div class="medium-6 large-6 cell grid-item">
+                                <div class="box grid-x grid-padding-x margin-bottom-2">
+                                    <div class="cell small-6">
+                                        <div class="thumb">
+                                            <?php $controlImageBefore = get_field('crf_before_image'); ?>
+                                            <?php if ($controlImageBefore): ?>
+                                                <img src="<?= esc_url( $controlImageBefore['url'] ); ?>" alt="<?= esc_attr( $controlImageBefore['alt'] ); ?>"  title="<?= esc_attr( $controlImageBefore['alt'] ); ?>">
+                                            <?php else: ?>
+                                                <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
+                                            <?php endif ?>
+                                            
+                                            <span>Before</span>
+                                        </div>
+                                    </div>
+                                    <div class="cell small-6">
+                                        <div class="thumb">
+                                             <?php $controlImageAfter = get_field('crf_after_image'); ?>
+                                            <?php if ($controlImageAfter): ?>
+                                                <img src="<?= esc_url( $controlImageAfter['url'] ); ?>" alt="<?= esc_attr( $controlImageAfter['alt'] ); ?>"  title="<?= esc_attr( $controlImageAfter['alt'] ); ?>">
+                                            <?php else: ?>
+                                                <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
+                                            <?php endif ?>
+                                            
+                                            <span>After</span>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="cell small-6">
-                                    <div class="thumb">
-                                         <?php $controlImageAfter = get_field('crf_after_image'); ?>
-                                        <?php if ($controlImageAfter): ?>
-                                            <img src="<?= esc_url( $controlImageAfter['url'] ); ?>" alt="<?= esc_attr( $controlImageAfter['alt'] ); ?>"  title="<?= esc_attr( $controlImageAfter['alt'] ); ?>">
-                                        <?php else: ?>
-                                            <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
-                                        <?php endif ?>
-                                        
-                                        <span>After</span>
-                                    </div>
+                                <h2 class="margin-bottom-2"><?php the_title(); ?></h2>
+                                <h3><?php the_field('crf_location') ?></h3>
+                                <ul>
+                                    <li>Initial Offer:</li>
+                                    <li><?php the_field('crf_initial_offer') ?></li>
+                                </ul>
+                                <ul>
+                                    <li>Final Award:</li>
+                                    <li><?php the_field('crf_final_award') ?></li>
+                                </ul>
+                                <p><?php the_field('crf_short_description') ?></p>
+                            </div>
+                            
+                        <?php endif ?>
+
+                        
+                    <?php endforeach; ?>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+
+            <?php else: ?>
+
+                <?php 
+                    $attorney =  array(
+                        'post_type' => 'attorney',
+                        'post_status' => 'publish',
+                        'posts_per_page' => -1,
+                    );
+
+                    $query = new WP_Query( $attorney );
+                    while ($query->have_posts()) : $query->the_post();
+                 ?>
+                    <div class="medium-6 large-6 cell grid-item">
+                        <div class="box grid-x grid-padding-x margin-bottom-2">
+                            <div class="cell small-6">
+                                <div class="thumb">
+                                    <?php $controlImageBefore = get_field('crf_before_image'); ?>
+                                    <?php if ($controlImageBefore): ?>
+                                        <img src="<?= esc_url( $controlImageBefore['url'] ); ?>" alt="<?= esc_attr( $controlImageBefore['alt'] ); ?>"  title="<?= esc_attr( $controlImageBefore['alt'] ); ?>">
+                                    <?php else: ?>
+                                        <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
+                                    <?php endif ?>
+                                    
+                                    <span><?= get_field('crf_before_image_text') ?></span>
                                 </div>
                             </div>
-                            <h2 class="margin-bottom-2"><?php the_title(); ?></h2>
-                            <h3><?php the_field('crf_location') ?></h3>
-                            <ul>
-                                <li>Initial Offer:</li>
-                                <li><?php the_field('crf_initial_offer') ?></li>
-                            </ul>
-                            <ul>
-                                <li>Final Award:</li>
-                                <li><?php the_field('crf_final_award') ?></li>
-                            </ul>
-                            <p><?php the_field('crf_short_description') ?></p>
+                            <div class="cell small-6">
+                                <div class="thumb">
+                                     <?php $controlImageAfter = get_field('crf_after_image'); ?>
+                                    <?php if ($controlImageAfter): ?>
+                                        <img src="<?= esc_url( $controlImageAfter['url'] ); ?>" alt="<?= esc_attr( $controlImageAfter['alt'] ); ?>"  title="<?= esc_attr( $controlImageAfter['alt'] ); ?>">
+                                    <?php else: ?>
+                                        <img src="https://via.placeholder.com/450x242/f0f0f0/cccccc?text=[no+thumnail]" alt="placeholder" title="placeholder" />
+                                    <?php endif ?>
+                                    
+                                    <span><?= get_field('crf_after_image_text'); ?></span>
+                                </div>
+                            </div>
                         </div>
-                        
-                    <?php endif ?>
+                        <h2 class="margin-bottom-2"><?php the_title(); ?></h2>
+                        <h3><?php the_field('crf_location') ?></h3>
+                        <ul>
+                            <li>Initial Offer:</li>
+                            <li><?php the_field('crf_initial_offer') ?></li>
+                        </ul>
+                        <ul>
+                            <li>Final Award:</li>
+                            <li><?php the_field('crf_final_award') ?></li>
+                        </ul>
+                        <p><?php the_field('crf_short_description') ?></p>
+                    </div>
 
+
+
+                <?php endwhile; wp_reset_postdata(); ?>
+            <?php endif ?>
                     
-                <?php endforeach; wp_reset_postdata(); ?>
-
-            <?php endwhile; wp_reset_postdata(); ?>
 
         </div>
     </div>

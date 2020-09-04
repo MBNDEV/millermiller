c02aa84e12b4e583c7ed044ac6a289bb5400f61b
elle-mybizniche <elle@mybizniche.com>
2020-09-04 12:16:55 +0800

    adding read more case-studies

--- taxonomy-case-studies.php
+++ taxonomy-case-studies.php
@@ -116,7 +116,7 @@ get_header();
                                 </div>
                                 
                                 <h2><a href="<?= get_the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                                <p><b>Issue:</b> <?= strip_tags(get_field('cs_issue')) ?></p>
+                                <p><b>Issue:</b> <?= strip_tags(get_field('cs_issue')) ?> <a href="<?= get_the_permalink(); ?>">...read more</a></p>
                                 
                             </div>
                         </article>

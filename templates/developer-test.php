f747d870cd7e1fb20f970745456fb5573f280dcf
elle-mybizniche <elle@mybizniche.com>
2020-09-10 15:04:12 +0800

    add developer test template

---
+++ templates/developer-test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+	Template Name: Developer Test Template
+ */
+
+get_header();
+?>
+
+<main id="content" class="contact-page">
+    <div class="wp-block-group sec-banner">
+        <div class="wp-block-group__inner-container">
+            <h1 class="hbg"><?php the_title(); ?></h1>
+            <h5>Start working with a law firm that <span>values clients and delivers results.</span></h5>
+        </div>
+    </div>
+</main>
+
+
+<?php
+
+wp_footer();

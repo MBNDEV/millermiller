9142df5b0810ff5aeeb730d476e82325a62d209a
elle-mybizniche <elle@mybizniche.com>
2020-09-10 12:12:38 +0800

    Update single-project-update.php

--- single-project-update.php
+++ single-project-update.php
@@ -4,11 +4,15 @@
 get_header();
 ?>
 <main id="content">
+    <div class="wp-block-group sec-banner">
+        <div class="wp-block-group__inner-container">
+            <h1 class="hbg"><?php the_title(); ?></h1>
+        </div>
+    </div>
 	<section class="page-content">
         <div class="grid-container">
             <?php
                 while ( have_posts() ) : the_post();
-                    the_title('<h1>', '</h1>');
                     the_content();
                 endwhile; // End of the loop.
             ?>      

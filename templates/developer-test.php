8c374b64fa1278628181976e63a60738e67e699e
elle-mybizniche <elle@mybizniche.com>
2020-09-10 15:11:43 +0800

    Update developer-test.php

--- templates/developer-test.php
+++ templates/developer-test.php
@@ -13,9 +13,44 @@ get_header();
             <h5>Start working with a law firm that <span>values clients and delivers results.</span></h5>
         </div>
     </div>
+
+    <div class="grid-container margin-vertical-3">
+
+        <div id="photo-search">
+            <h2>Search Photos:</h2>
+            <form action="<?php the_permalink(); ?>" id="searchform" method="get">
+                <div>
+                    <label for="s" class="screen-reader-text">Search for:</label>
+                    <input type="text" id="search" name="search" value="" />
+                    <input type="hidden" name="post_type" value="post">
+                    <input type="hidden" name="post_type" value="case-study">
+                    <input type="submit" value="Search" id="searchsubmit" />
+                </div>
+            </form>
+
+            <?php 
+                if( isset( $_REQUEST['search'] ) ) {
+                    query_posts( array(
+                        's' => $_REQUEST['search'],
+                        'post_type' =>  array($_REQUEST['post'], $_REQUEST['case-study']),
+                        'paged' => $paged
+                    ));
+
+                    if( have_posts() ) : while ( have_posts() ) :
+                        the_title();
+                        the_content();                      
+                    endwhile; endif;
+
+                    wp_reset_query(); 
+                }
+            ?>
+        </div>
+
+
+    </div>
 </main>
 
 
 <?php
 
-wp_footer();
+get_footer();

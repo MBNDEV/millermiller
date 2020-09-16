0ad31b2ed7400cef1262cd58b78ad0e75973617b
elle-mybizniche <elle@mybizniche.com>
2020-09-16 14:06:05 +0800

    Create search-template.php

---
+++ templates/search-template.php
@@ -0,0 +1,34 @@
+<?php
+/**
+	Template Name: Search Results Template
+ */
+
+get_header();
+?>
+
+<main id="content">
+
+	<div class="wp-block-group sec-banner">
+        <div class="wp-block-group__inner-container">
+            <h1 class="hbg">Search Results</h1>
+        </div>
+    </div>
+
+    <div class="grid-container">
+
+
+ 		<div class="grid-x grid-margin-x align-center">
+
+    		<div class="cell small-12 medium-12">
+
+
+				
+    		</div>
+    	</div>
+    </div>
+	
+</main>
+
+<?php
+
+get_footer();
